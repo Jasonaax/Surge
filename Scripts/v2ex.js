70cfe701481d95783e8b10a921db1e3b1aa9365c
Jason <129001357+Jasonaax@users.noreply.github.com>
2026-04-08 16:25:56 +0800

    Create v2ex.js

---
+++ Scripts/v2ex.js
@@ -0,0 +1,10 @@
+// 2026/4/8 05:48:15 https://raw.githubusercontent.com/ElementRef/AboutConfig/main/script/ddgksf2013.startup.v2ex.js
+var body=$response.body.replace(/<head>/,`<head>
+    <style>
+      .sidebar_units,
+      .sidebar_compliance,
+      ins.adsbygoogle,
+      div[class^="wwads-"]{
+        display: none !important;
+      }
+    </style>`);$done({body});
